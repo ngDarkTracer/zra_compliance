dca92b1290ec16bc3bf0f04e41830b28716760ba
ngDarkTracer <lorelkouam@gmail.com>
2025-03-19 17:35:53 +0100

    update.js

--- update.js
+++ update.js
@@ -63,67 +63,71 @@ app.post(`/credit_note`, async (req, res) => {
             .whereBetween('creation_date', [startDate, endDate]))
             .map(credit_note => credit_note.id);
 
-        // Récupérer les ticket_numbers associés aux credit_note
-        const ticket_numbers_ids = (await knex.select(knex.raw('ARRAY_AGG(ticket_number) as ticket_numbers'))
-            .from('air_booking')
-            .whereIn('id_credit_note', credit_notes_ids))[0]?.ticket_numbers;
+        if (credit_notes_ids.length) {
+            // Récupérer les ticket_numbers associés aux credit_note
+            const ticket_numbers_ids = (await knex.select(knex.raw('ARRAY_AGG(ticket_number) as ticket_numbers'))
+                .from('air_booking')
+                .whereIn('id_credit_note', credit_notes_ids))[0]?.ticket_numbers;
 
-        const air_bookings = (await knex.select('ticket_number', knex.raw('ARRAY_AGG(ROW_TO_JSON(air_booking)) AS bookings'))
-            .from('air_booking')
-            .whereIn('ticket_number', ticket_numbers_ids)
-            .groupBy('ticket_number'))
-            ?.map(air_booking => ({
+            const air_bookings = (await knex.select('ticket_number', knex.raw('ARRAY_AGG(ROW_TO_JSON(air_booking)) AS bookings'))
+                .from('air_booking')
+                .whereIn('ticket_number', ticket_numbers_ids)
+                .groupBy('ticket_number'))
+                ?.map(air_booking => ({
+                    ...air_booking,
+                    bookings: air_booking?.bookings?.filter(travel_item => (travel_item?.id_invoice || travel_item?.id_credit_note))
+                }))
+                .reduce((acc, air_booking) => {
+                    let linkedInvoice = air_booking.bookings.find(ab => ab.id_invoice);
+                    acc.push(...air_booking.bookings.map(ab => ({
+                        ...ab,
+                        id: linkedInvoice?.id,
+                        id_invoice: linkedInvoice?.id_invoice || ab.id_invoice
+                    })));
+                    return acc;
+                }, [])
+                .filter(air_booking => air_booking.id_credit_note && air_booking.id_invoice);
+
+            // Récupérer les IDs des invoices
+            const invoices_ids = air_bookings.map(air_booking => air_booking.id_invoice);
+
+            // Récupérer les informations des invoices
+            const invoices_numbers = (await knex.select('id', 'invoice_number', 'zra_invoice_id')
+                .from('invoice')
+                .whereIn('id', invoices_ids))
+                .reduce((acc, { id, invoice_number, zra_invoice_id }) => {
+                    acc[id] = { invoice_number, zra_invoice_id };
+                    return acc;
+                }, {});
+
+            // Reformater les air_booking
+            const reformatted_air_bookings = air_bookings.map(air_booking => ({
                 ...air_booking,
-                bookings: air_booking?.bookings?.filter(travel_item => (travel_item?.id_invoice || travel_item?.id_credit_note))
-            }))
-            .reduce((acc, air_booking) => {
-                let linkedInvoice = air_booking.bookings.find(ab => ab.id_invoice);
-                acc.push(...air_booking.bookings.map(ab => ({
-                    ...ab,
-                    id: linkedInvoice?.id,
-                    id_invoice: linkedInvoice?.id_invoice || ab.id_invoice
-                })));
-                return acc;
-            }, [])
-            .filter(air_booking => air_booking.id_credit_note && air_booking.id_invoice);
+                zra_invoice_id: invoices_numbers[air_booking.id_invoice]?.zra_invoice_id,
+                id_invoice: invoices_numbers[air_booking.id_invoice]?.invoice_number
+            }));
 
-        // Récupérer les IDs des invoices
-        const invoices_ids = air_bookings.map(air_booking => air_booking.id_invoice);
+            // Regrouper par ID de note de crédit
+            const grouped_credits_notes = groupBy(reformatted_air_bookings, 'id_credit_note');
 
-        // Récupérer les informations des invoices
-        const invoices_numbers = (await knex.select('id', 'invoice_number', 'zra_invoice_id')
-            .from('invoice')
-            .whereIn('id', invoices_ids))
-            .reduce((acc, { id, invoice_number, zra_invoice_id }) => {
-                acc[id] = { invoice_number, zra_invoice_id };
-                return acc;
-            }, {});
+            const total = (await knex.count('* as total')
+                .from('credit_note')
+                .whereNull('zra_id')
+                .whereIn('id', Object.keys(grouped_credits_notes)))[0].total || 0
 
-        // Reformater les air_booking
-        const reformatted_air_bookings = air_bookings.map(air_booking => ({
-            ...air_booking,
-            zra_invoice_id: invoices_numbers[air_booking.id_invoice]?.zra_invoice_id,
-            id_invoice: invoices_numbers[air_booking.id_invoice]?.invoice_number
-        }));
-
-        // Regrouper par ID de note de crédit
-        const grouped_credits_notes = groupBy(reformatted_air_bookings, 'id_credit_note');
-
-        const total = (await knex.count('* as total')
-            .from('credit_note')
-            .whereNull('zra_id')
-            .whereIn('id', Object.keys(grouped_credits_notes)))[0].total || 0
-
-        // Récupérer les notes de crédit correspondantes
-        const credit_notes = (await knex.select('credit_note.id', 'credit_note.number', 'credit_note.creation_date', 'credit_note.amount', 'credit_note.status', 'credit_note.balance','customer.customer_name')
-            .from('credit_note')
-            .join('customer', 'customer.id', 'credit_note.id_customer')
-            .groupBy('credit_note.id', 'customer.id')
-            .whereIn('credit_note.id', Object.keys(grouped_credits_notes))
-            .limit(pageSize)
-            .offset(pageSize * (pageNumber - 1)))
-            ?.map(credit_note => credit_note)?.filter(credit_note => credit_note);
-        res.json({ credit_notes, response_message, total })
+            // Récupérer les notes de crédit correspondantes
+            const credit_notes = (await knex.select('credit_note.id', 'credit_note.number', 'credit_note.creation_date', 'credit_note.amount', 'credit_note.status', 'credit_note.balance','customer.customer_name')
+                .from('credit_note')
+                .join('customer', 'customer.id', 'credit_note.id_customer')
+                .groupBy('credit_note.id', 'customer.id')
+                .whereIn('credit_note.id', Object.keys(grouped_credits_notes))
+                .limit(pageSize)
+                .offset(pageSize * (pageNumber - 1)))
+                ?.map(credit_note => credit_note)?.filter(credit_note => credit_note);
+            res.json({ credit_notes, response_message, total })
+        } else {
+            res.json({ credit_notes: [], response_message, total: 0 })
+        }
     } catch (error) {
         res.status(500).end(error.message)
     }
